7af884c0549f73c714a009edfe786e56aef6e35e
NiklasBrue <50956653+NiklasBrue@users.noreply.github.com>
2022-03-18 11:56:42 +0100

    Delete Game_Of_Life_ directory

--- Game_Of_Life_/sample/constants.py
+++
@@ -1,5 +0,0 @@
-height = width = 400
-cube_size = 10
-
-black = (0, 0, 0)
-white = (255, 255, 255)
--- Game_Of_Life_/sample/helper.py
+++
@@ -1,7 +0,0 @@
-from . import constants
-
-
-def mouse_position_to_board_position(mouse_position):
-    array_col = mouse_position[0] // constants.cube_size
-    array_row = mouse_position[1] // constants.cube_size
-    return array_row, array_col
